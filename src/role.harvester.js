9d4c807e8d6b3b2cfc17dc3e52e5d322100297b3
Grant Smith <grantsmith55555@gmail.com>
2018-07-01 04:12:10 -0700

    day1

---
+++ src/role.harvester.js
@@ -0,0 +1,44 @@
+var roleHarvester = {
+
+    /** @param {Creep} creep **/
+    run: function(creep) {
+	    if(creep.carry.energy < creep.carryCapacity) {
+            var sources = creep.room.find(FIND_SOURCES);
+            
+            if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+            
+            for (name in Game.creeps){
+                if (creep !== Game.creeps[name]){
+                    if(Game.creeps[name].pos === Game.flags['Flag1'].pos){
+                        if(creep.harvest(sources[1]) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(sources[1], {visualizePathStyle: {stroke: '#ffaa00'}});
+                        }
+                    }
+                    
+                    
+                }
+            }
+            
+        }
+        else {
+            var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity;
+                    }
+            });
+            if(targets.length > 0) {
+                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                }
+            }
+            else{
+                creep.moveTo(Game.spawns['Spawn1']);
+            }
+        }
+	}
+};
+
+module.exports = roleHarvester;
